b6f6b3db1f6d1c8dcc3289b624f85ce0133ea110
Pangpyo <kkp0639@gmail.com>
2023-09-04 14:24:27 +0900

    🎉 init: 프로젝트 시작 및 환경 설정

---
+++ antoday/src/test/java/com/omfg/auntoday/AntodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omfg.auntoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
