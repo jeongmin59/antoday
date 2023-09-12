75d616bdbf2a03a852a87816d62338b97b2833d4
subin <osb4469@naver.com>
2023-09-12 18:00:32 +0900

    🐛 Fix: 검색화면 결과 수정

--- antoday_FE/src/components/Common/atoms/UpperNavBar.tsx
+++ antoday_FE/src/components/Common/atoms/UpperNavBar.tsx
@@ -3,6 +3,7 @@ import styles from './UpperNavBar.module.css'
 import { Link } from 'react-router-dom';
 
 const UpperNavBar = () => {
+  console.log('확인')
   return (
     <nav className={styles.navigation}>
       <Link to='/' className={styles.navItem}>홈화면</Link>
