9fb7d6db4d199f562b0d146fa3a2e3e0082cb182
SEOIHYEON <tjwn1408@naver.com>
2023-09-15 11:19:11 +0900

    :sparkles: 홈화면 날짜 및 텍스트 컴포넌트 구현 완료

--- antoday_FE/src/components/WordCloud/atoms/TitleTextComponent.tsx
+++ antoday_FE/src/components/WordCloud/atoms/TitleTextComponent.tsx
@@ -1,8 +1,24 @@
+import React from 'react';
 import styles from './TitleTextComponent.module.css'
 
-const TitleTextComponent = () => {
+const TitleTextComponent : React.FC = () => {
+
+    const getCurrentDate = () => {
+        const daysOfWeek = ['일','월','화','수', '목', '금', '토'];
+        const today = new Date();
+        const dayOfWeek = daysOfWeek[today.getDay()];
+        const year = today.getFullYear();
+        const month = (today.getMonth() + 1).toString().padStart(2,'0');
+        const day = today.getDate().toString().padStart(2,'0');
+
+        return `${year}-${month}-${day} ${dayOfWeek}요일`
+    }
+
     return ( 
-        <>제목과 텍스트가 있는 컴포넌트</>
+        <div className={styles.titleTextContainer}>
+        <div className = {styles.title}>{getCurrentDate()}</div>
+        <div className = {styles.text}>오늘 주목해야 할 키워드</div>
+        </div>
     );
 }
 
