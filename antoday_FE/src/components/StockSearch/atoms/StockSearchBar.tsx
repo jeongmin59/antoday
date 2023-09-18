01c1dab27ca33e1881758196bfadde50018ce43f
SEOIHYEON <tjwn1408@naver.com>
2023-09-18 17:09:36 +0900

    :sparkles: Feat: 검색 결과 구현중

--- antoday_FE/src/components/StockSearch/atoms/StockSearchBar.tsx
+++ antoday_FE/src/components/StockSearch/atoms/StockSearchBar.tsx
@@ -4,25 +4,32 @@ import { useQuery, useQueryClient } from "react-query";
 import styles from "./StocksSearchBar.module.css";
 import axios from "axios";
 import SearchBottomSheet from "./SearchBottomSheet";
+import useDebounce from "../../../utils/useDebounce";
 
 const StockSearchBar: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [currentPage, setCurrentPage] = useState<number>(0);
   const [totalPage, setTotalPage] = useState<number>(0);
   const queryClient = useQueryClient();
+
+  const debouncedInputValue = useDebounce({
+    value: inputValue,
+    delay: 300, // 디바운스 딜레이 설정 (예: 300ms)
+  });
 
   useEffect(() => {
     // console.log('입력값이 변경되었습니다:', inputValue);
     // console.log('전체페이지수는',totalPage);
     queryClient.invalidateQueries("searchResults");
-    console.log("제발나와줘", totalPage);
-  }, [inputValue, totalPage]);
+    // console.log("제발나와줘", totalPage);
+  }, [debouncedInputValue, currentPage]);
 
   const {
     data: searchResults,
     isLoading,
     isError,
   } = useQuery(
-    ["searchResults", inputValue],
+    ["searchResults", inputValue, currentPage],
     async () => {
       if (!inputValue) {
         // inputValue가 빈 문자열인 경우 요청 보내지 않음
@@ -58,9 +65,22 @@ const StockSearchBar: React.FC = () => {
     queryClient.invalidateQueries("searchResults");
   };
 
+  const handlePreviousPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPage - 1) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
   const handleSubmit = async (event: React.FormEvent) => {
     //기본 제출 동작 방지
     event.preventDefault();
+    setCurrentPage(0); // 검색 시 첫 페이지로 리셋
     await search();
   };
 
@@ -79,8 +99,20 @@ const StockSearchBar: React.FC = () => {
           search
         </button>
       </form>
-      {inputValue && (
-        <SearchBottomSheet keyword={inputValue} totalPage={totalPage} />
+
+      {totalPage > 0 && (
+        <div className={styles.pagination}>
+          <button onClick={handlePreviousPage} disabled={currentPage === 0}>
+            이전 페이지
+          </button>
+          <span>페이지 {currentPage + 1}</span>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPage - 1}
+          >
+            다음 페이지
+          </button>
+        </div>
       )}
     </React.Fragment>
   );
