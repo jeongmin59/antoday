802ec8acea2eebe43272eb5be394774d7518d841
hyemco <hyemco@gmail.com>
2023-09-18 17:15:21 +0900

    ✨ Feat: KOSPI/KOSDAQ 지수 조회 기능 구현
    지수 기준일자, 현재 지수, 등락률, 전날과의 지수 차이 값 포함

--- antoday_data/app/routers/price.py
+++ antoday_data/app/routers/price.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
+from app.schemas.price import MarketInfoDTO
+from app.services.price_service import get_market_info
 
 router = APIRouter(
     prefix="/info/price",
     tags=["price"]
 )
 
-@router.get("/KSQSTK")
+@router.get("/KSQSTK", response_model=MarketInfoDTO)
 async def get_value_KSQSTK():
-    return {"message": "test"}
+    response_data = get_market_info("KS11", "KQ11")
+    return response_data
