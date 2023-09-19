04996d70f5917f65b4e289cd94619873883647d8
hyemco <hyemco@gmail.com>
2023-09-19 11:09:59 +0900

    🐛 Fix: CORS 설정 추가
    현재는 모든 origins, method, header 허용

--- antoday_data/app/main.py
+++ antoday_data/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.routers import price
 from app.routers import corp
 
 app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"]
+)
 
 app.include_router(price.router)
 app.include_router(corp.router)
