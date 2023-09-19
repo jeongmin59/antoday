fe530740c8e1d640211a36f4a7a30d631d4c3d99
young eun <duddms0190@naver.com>
2023-09-19 12:41:09 +0900

    ✨ Feat: 주가 데이터 조회 api 추가

--- antoday_data/app/routers/corp.py
+++ antoday_data/app/routers/corp.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from app.schemas.price import MarketInfoDTO
 from app.services.price_service import get_market_info
+from app.services.stock_info_service import get_stock_price
 
 router = APIRouter(
     prefix="/info/corp",
@@ -12,4 +13,12 @@ async def get_value_KSQSTK():
     response_data = {
         "msg":"sample api"
     }
+    return response_data
+
+@router.get("/stock")
+async def get_stock_pricedata(
+    stock_code: str = Query(regex="^[0-9]+$"),
+    date_option: str = Query()
+):
+    response_data = get_stock_price(stock_code, date_option)
     return response_data
