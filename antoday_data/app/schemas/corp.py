3cf3554e3794c2046f434c935fbee470eaa4b02e
hyemco <hyemco@gmail.com>
2023-09-20 17:42:13 +0900

    ✨ Feat: 메인 HOT 기업 response 수정
    주가, 등락률, 거래량 response에서 제외(속도개선)

--- antoday_data/app/schemas/corp.py
+++ antoday_data/app/schemas/corp.py
@@ -3,8 +3,4 @@ from pydantic import BaseModel
 class CorpListDTO(BaseModel):
     stock_code: str
     corp_name: str
-    price: int
-    change: int
-    change_percent: float
-    volume: int
     logo_url: str
--- antoday_data/app/services/corp_service.py
+++ antoday_data/app/services/corp_service.py
@@ -26,9 +26,7 @@ def get_hot_corp_list():
 
         corpList.sort(key=lambda x: x["volume"], reverse=True)
         corpList = corpList[:10]
-    
 
-    
     corpListDTO = [CorpListDTO(**corp) for corp in corpList]
     
     return corpListDTO
@@ -62,29 +60,11 @@ def scraping_hot_corp(status):
             if len(columns) >= 11:
                 rank = columns[0].get_text().strip()
                 name = columns[1].find('a', class_='tltle').get_text().strip()
-                price = columns[2].get_text().strip().replace(',', '')
-                change = columns[3].get_text().strip().replace(',', '')
-                change_percent_text = columns[4].get_text().strip()
-                volume = columns[5].get_text().strip().replace(',', '')
-
-                # "+" 기호가 있는지 확인 후 제거
-                if '+' in change_percent_text:
-                    change_percent_text = change_percent_text.strip('+')
-
-                # 백분율 기호(%) 제거
-                change_percent = float(change_percent_text.strip('%'))
-
-                # 숫자로 변환
-                price = int(price)
-                change = int(change)
-                volume = int(volume)
+                volume = int(columns[5].get_text().strip().replace(',', ''))
                 
                 corp_info = {
                     "rank": rank,
                     "corp_name": name,
-                    "price": price,
-                    "change": change,
-                    "change_percent": change_percent,
                     "volume": volume,
                 }
 
