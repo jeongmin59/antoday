2a4a14419a40b592c1c09ffb61ad61b0f8495020
Pangpyo <kkp0639@gmail.com>
2023-09-20 15:55:04 +0900

    👷 Chore: Dockerfile 추가

--- antoday_data/app/models/models.py
+++ antoday_data/app/models/models.py
@@ -1,11 +1,18 @@
 from datetime import datetime
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, TEXT
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
 class Textmining(Base):
     __tablename__ = "textmining"
-    pk = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    textmining_pk = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, onupdate=datetime.now)
+
+class News(Base):
+    __tablename__ = "news"
+    news_pk = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    url = Column(TEXT)
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, onupdate=datetime.now)
