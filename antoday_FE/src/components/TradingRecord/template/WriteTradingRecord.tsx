ca48ef29a5a2310a45755b5fa50316950ee5bd73
subin <osb4469@naver.com>
2023-09-22 17:08:01 +0900

    ✨ Feat: 주가 받아오기 최종 완료 및 데이터 전달

--- antoday_FE/src/components/TradingRecord/template/WriteTradingRecord.tsx
+++ antoday_FE/src/components/TradingRecord/template/WriteTradingRecord.tsx
@@ -22,7 +22,7 @@ interface Company {
 const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWritePage }) => {
   const navigate = useNavigate();
   const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedOption, setSelectedOption] = useState<string>('');
+  const [selectedOption, setSelectedOption] = useState<string>('매수');
   const [searchResults, setSearchResults] = useState<any[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
@@ -32,8 +32,9 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
   const [adjustedPrice, setAdjustedPrice] = useState<number | null>(null);
   const [stockQuantity, setStockQuantity] = useState(0);
   const [ownedCompanies, setOwnedCompanies] = useState<Company[]>([]);
-  const [token,setToken] = useRecoilState(accessTokenAtom);
+  const [token, setToken] = useRecoilState(accessTokenAtom);
   const [forceRender, setForceRender] = useState(0);
+  const [alertMessage, setAlertMessage] = useState<string | null>(null);
 
 
   const tradingData = {
@@ -44,11 +45,62 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
     stockQuantity,
   };
 
-  const gotowritetradingrecord = () => {
-    navigate('/writetradingrecord/:recordPk', { state: { 
-      tradingdata: tradingData,
-     } });
+  const formatDateToCustom = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = "03";
+    const minutes = "41";
+    const seconds = "16";
+    
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+  }
+  
+
+  const gotowritetradingrecord = async () => {
+    let errorMessage = null;
+  
+    if (!selectedCompany) {
+      errorMessage = "회사를 선택해주세요.";
+      console.log('회사선택안함');
+    } else if (stockQuantity <= 0) {
+      errorMessage = "주식 개수를 지정해주세요.";
+      console.log('주식개수안함');
+    }
+    if (errorMessage) {
+      setAlertMessage(errorMessage);
+      // return;
+    }
+  
+    const apiUrl = `${import.meta.env.VITE_BACK_API_URL}/api/trade`;
+    console.log(tradingData.stockQuantity,  selectedOption === '매도' ? 1 : 0, tradingData.adjustedPrice, selectedCompany?.stockCode, formatDateToCustom(selectedDate))
+    try {
+      const response = await axios.post(apiUrl, {
+        cnt: tradingData.stockQuantity,
+        optionBuySell: selectedOption === '매도' ? 1 : 0,
+        price: tradingData.adjustedPrice,
+        stockCode: selectedCompany?.stockCode,
+        tradeAt: formatDateToCustom(selectedDate),
+        keywords: [
+          "string"
+        ],
+        reason: "string",
+      }, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      });
+      const tradePk = response.data.tradePk;
+      console.log("확인", tradePk)
+      navigate(`/writetradingrecord/${tradePk}`);
+      setAlertMessage(null);
+      return true;
+    } catch (error) {
+      console.error('Fetch stock price error:', error);
+      return false;
+    }
   };
+  
 
   const getIncrementValue = () => {
     if (adjustedPrice === null) return 0;
@@ -60,7 +112,10 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
 };
 
   const fetchOwnedCompanies = () => {
-    axios.get(`${import.meta.env.VITE_BACK_API_URL}/api/trade/corp`)
+    axios.get((`${import.meta.env.VITE_BACK_API_URL}/api/trade/corp`),
+    {headers : {
+      Authorization: `Bearer ${token}`
+    }})
     .then((response) => {
       setOwnedCompanies(response.data);
       // console.log(ownedCompanies)
@@ -73,7 +128,7 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
   const fetchStockPrice = (stockCode: number, status: string) => {
     let apiUrl = '';
     const formattedDate = `${selectedDate.getFullYear()}-${String(selectedDate.getMonth() + 1).padStart(2, '0')}-${String(selectedDate.getDate()).padStart(2, '0')}`;
-
+    console.log(selectedDate)
     if (status === '매수') {
       apiUrl = `${import.meta.env.VITE_DATA_API_URL}/corp/price/${stockCode}`; 
       axios.get(apiUrl, {
@@ -88,7 +143,7 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
         const fetchedPrice = response.data.price;
         setStockPrice(fetchedPrice);
         setAdjustedPrice(fetchedPrice);
-        console.log(fetchedPrice);
+        // console.log(fetchedPrice);
       })
       .catch((error) => {
         console.error('Fetch stock price error:', error);
@@ -96,23 +151,27 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
     } else if (status === '매도') {
       apiUrl = `${import.meta.env.VITE_BACK_API_URL}/api/corp/price`; 
       axios.get(apiUrl, {
-        params: {
-          status: status,
-        },
-        headers: {
-          Authorization : `Bearer ${token}`
-        }
+          params: {
+              status: status,
+          },
+          headers: {
+              Authorization : `Bearer ${token}`
+          }
       })
       .then((response) => {
-        const fetchedPrice = response.data.price;
-        setStockPrice(fetchedPrice);
-        setAdjustedPrice(fetchedPrice);
-        console.log(fetchedPrice);
+          const companyData = response.data.find((company: any) => company.corpName === selectedCompany?.corpName);
+          if (companyData) {
+              const fetchedPrice = companyData.lastBuyPrice;
+              setStockPrice(fetchedPrice);
+              setAdjustedPrice(fetchedPrice);
+          } else {
+              console.error("Couldn't find matching company data for selected company.");
+          }
       })
       .catch((error) => {
-        console.error('Fetch stock price error:', error);
+          console.error('Fetch stock price error:', error);
       });
-    }
+  }
 
     
   };
@@ -149,7 +208,7 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
     .then((response) => {
       const { content, totalPages } = response.data;
       setSearchResults(content);
-      console.log(response.data)
+      // console.log(response.data)
       setTotalPages(totalPages);
     })
     .catch((error) => {
@@ -169,10 +228,13 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
     handleSearchCompany(searchKeyword);
   };
 
-  const handleButtonClick = () => {
-    closeWritePage();
-    gotowritetradingrecord();
+  const handleButtonClick = async () => {
+    const success = await gotowritetradingrecord();
+    if (success) {
+        closeWritePage();
+    }
   };
+
 
   const handleClick = (option: string) => {
     if (selectedOption !== option) {
@@ -218,74 +280,82 @@ const WriteTradingRecordPage: React.FC<WriteTradingRecordPageProps> = ({ closeWr
       </div>
   
       <div>
-        {ownedCompanies.length > 0 && !selectedCompany ? (
-          <div>
-            <h2>보유한 주식</h2>
-            {ownedCompanies.map((company) => (
-              <div 
-                key={company.stockCode} 
-                className={styles.corpcontainer}
-                onClick={() => handleSelectCompany(company)}
-              >
-                <img src={company.logoUrl} alt={company.corpName} />
-                <span>{company.corpName}</span>
-              </div>
-            ))}
-          </div>
-        ) : !selectedCompany ? (
-          <div>
-            <SearchingCompany onSearch={handleSearchCompany} />
-            <div className={styles.searchresults}>
-              {searchResults.map((result) => (
-                <div
-                  key={result.stockCode}
-                  className={styles.corpcontainer}
-                  onClick={() => handleSelectCompany(result)}
-                >
-                  <img src={result.logoUrl} alt={result.corpName} />
-                  <span>{result.corpName}</span>
-                </div>
-              ))}
-              {Array.from({ length: totalPages }, (_, index) => (
-                <button key={index} onClick={() => handlePageChange(index + 1)}>
-                  {index + 1}
-                </button>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <div key={selectedCompany.stockCode} className={styles.corpcontainer}>
-            <img src={selectedCompany.logoUrl} alt={selectedCompany.corpName} />
-            <span>{selectedCompany.corpName}</span>
-          </div>
-        )}
+      {selectedCompany ? (
+  <div key={selectedCompany?.stockCode} className={styles.corpcontainer}>
+    <img src={selectedCompany?.logoUrl} alt={selectedCompany?.corpName} />
+    <span>{selectedCompany?.corpName}</span>
+  </div>
+) : ownedCompanies.length > 0 && selectedOption === '매도' ? (
+  <div>
+    <h2>보유한 주식</h2>
+    {ownedCompanies.map((company) => (
+      <div 
+        key={company.stockCode} 
+        className={styles.corpcontainer}
+        onClick={() => handleSelectCompany(company)}
+      >
+        <img src={company.logoUrl} alt={company.corpName} />
+        <span>{company.corpName}</span>
+      </div>
+    ))}
+  </div>
+) : !selectedCompany && selectedOption === '매수' ? (
+  <div>
+    <SearchingCompany onSearch={handleSearchCompany} />
+    <div className={styles.searchresults}>
+      {searchResults.map((result) => (
+        <div
+          key={result.stockCode}
+          className={styles.corpcontainer}
+          onClick={() => handleSelectCompany(result)}
+        >
+          <img src={result.logoUrl} alt={result.corpName} />
+          <span>{result.corpName}</span>
+        </div>
+      ))}
+      {Array.from({ length: totalPages }, (_, index) => (
+        <button key={index} onClick={() => handlePageChange(index + 1)}>
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  </div>
+) : (
+  <div>보유주식이 없습니다.</div>
+)}
+
   
-        <div>
-        <input
-          type="number"
-          value={stockQuantity === 0 ? "" : stockQuantity}
-          onChange={(e) => {
-              const value = e.target.value;
-              if (value === "") {
-                  setStockQuantity(0);
-              } else {
-                  setStockQuantity(parseInt(value, 10));
-              }
-          }}
-          // placeholder="0"
+  {!(selectedOption === '매도' && ownedCompanies.length === 0) && (
+  <>
+    <div>
+      <input
+        type="number"
+        value={stockQuantity === 0 ? "" : stockQuantity}
+        onChange={(e) => {
+          const value = e.target.value;
+          if (value === "") {
+              setStockQuantity(0);
+          } else {
+              setStockQuantity(parseInt(value, 10));
+          }
+      }}
       />
-          주
-        </div>
-        <div className={styles.horizontal}>
-          <div>평단가</div>
-          <div>
-            <button onClick={() => adjustPrice(-getIncrementValue())}>-</button>
-            {adjustedPrice !== null ? `${adjustedPrice}원` : "0 원"}
-            <button onClick={() => adjustPrice(getIncrementValue())}>+</button>
-          </div>
-        </div>
-        <button onClick={closeWritePage}>취소</button>
-        <button onClick={handleButtonClick}>추가</button>
+      주
+    </div>
+    <div className={styles.horizontal}>
+      <div>평단가</div>
+      <div>
+        <button onClick={() => adjustPrice(-getIncrementValue())}>-</button>
+        {adjustedPrice !== null ? `${adjustedPrice}원` : "0 원"}
+        <button onClick={() => adjustPrice(getIncrementValue())}>+</button>
+      </div>
+    </div>
+    <button onClick={closeWritePage}>취소</button>
+    <button onClick={handleButtonClick}>추가</button>
+    {alertMessage && <p>{alertMessage}</p>}
+  </>
+)}
+
       </div>
     </div>
   );
