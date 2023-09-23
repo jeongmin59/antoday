21dfec7030d093422bd096d43adad4e4373766fc
subin <osb4469@gmail.com>
2023-09-24 04:57:35 +0900

    ✨ Feat: 관심기업/투자기업 컴포넌트

---
+++ antoday_FE/src/components/StockSearch/atom/FavoriteStockComponent.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./FavoriteStockComponent.module.css";
+import { useNavigate } from "react-router-dom";
+import FavoriteCompanyButton from "../../StockInfo/atom/FavoriteCompanyButton";
+
+interface StockInfoComponentProps {
+  companyInfo: CompanyInfo; // 기업 정보를 받을 prop
+//   isLoading: boolean;
+}
+
+const FavoriteStockComponent: React.FC<StockInfoComponentProps> = ({
+  companyInfo,
+}) => {
+  const navigator = useNavigate();
+
+  const handleClick = () => {
+    const stockCode = companyInfo.stockCode;
+    navigator(`/stockinfo/${stockCode}`);
+  };
+
+  return (
+    <div className={styles.stockSearchResult} onClick={handleClick}>
+      {companyInfo ? (
+        <React.Fragment>
+          <img
+            className={styles.stockImage}
+            src={companyInfo.logoUrl}
+            alt="기업이미지"
+          />
+          <p>{companyInfo.corpName}</p>
+          <FavoriteCompanyButton stockPk={companyInfo.stockCode} />
+        </React.Fragment>
+      ) : (
+        // 스켈레톤 코드를 표시
+        <div className={styles.skeleton}>
+          <div className={styles.skeletonImage}></div>
+          <div className={styles.skeletonText}></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FavoriteStockComponent;
