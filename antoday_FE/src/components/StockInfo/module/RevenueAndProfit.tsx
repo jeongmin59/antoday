eb36c32ce9e9cd227afd95ad4caddef49f273d2a
공정민 <jmgong59@gmail.com>
2023-09-24 22:12:14 +0900

    ✨ Feat: 매출액, 영업이익 컴포넌트 구현 중

--- antoday_FE/src/components/StockInfo/module/RevenueAndProfit.tsx
+++ antoday_FE/src/components/StockInfo/module/RevenueAndProfit.tsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./RevenueAndProfit.module.css";
+import ReactApexChart from 'react-apexcharts';
+import axios from 'axios';
 
 const RevenueAndProfit: React.FC = () => {
+  const [chartData, setChartData] = useState<any>({
+    takes: [],
+    profits: []
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          import.meta.env.VITE_DATA_API_URL + '/corp/overview', 
+        {
+          params: {
+            stock_code: `${stock_code}`
+          }
+        });
+        setChartData(response.data.value);
+        console.log(response)
+
+      } catch (error) {
+        console.error('에러', error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  const chartOptions = {
+    chart: {
+      type: 'bar'
+    },
+    xaxis: {
+      categories: chartData.takes.map((item: any) => item.year)
+    }
+  };
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.title}>매출액 및 영업이익</div>
-      <div className={styles.graph}>여기에 차트가 들어갈거</div>
+      <div className={styles.graph}>
+        <ReactApexChart options={chartOptions} series={[{
+          name: '매출',
+          data: chartData.takes.map((item: any) => item.take)
+        }, {
+          name: '영업이익',
+          data: chartData.profits.map((item: any) => item.prpfit)
+        }]} type="bar" />
+      </div>
     </div>
   );
 };
