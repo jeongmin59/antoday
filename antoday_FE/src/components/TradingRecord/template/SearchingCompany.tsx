c2fd9fab159e462577e407f5b5721f55f500248e
subin <osb4469@gmail.com>
2023-09-25 06:03:43 +0900

    ✨ Feat: 매매기록 작성 로직 수정 및 날짜 이슈 수정

--- antoday_FE/src/components/TradingRecord/template/SearchingCompany.tsx
+++ antoday_FE/src/components/TradingRecord/template/SearchingCompany.tsx
@@ -8,9 +8,17 @@ interface SearchInputProps {
 
 const SearchingCompany: React.FC<SearchInputProps> = ({ onSearch }) => {
     const [keyword, setKeyword] = useState('');
-
+    const [shouldClearOnNextFocus, setShouldClearOnNextFocus] = useState(false); 
     const handleSearch = () => {
         onSearch(keyword);
+        setShouldClearOnNextFocus(true);
+    };
+
+    const handleInputFocus = () => {
+        if (shouldClearOnNextFocus) {
+            setKeyword('');
+            setShouldClearOnNextFocus(false);
+        }
     };
 
     return (
@@ -20,6 +28,7 @@ const SearchingCompany: React.FC<SearchInputProps> = ({ onSearch }) => {
                 placeholder="종목명 검색"
                 value={keyword}
                 onChange={(e) => setKeyword(e.target.value)}
+                onFocus={handleInputFocus}
             />
             <button onClick={handleSearch}>검색</button>
         </div>
