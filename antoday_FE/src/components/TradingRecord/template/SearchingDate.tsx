137d0d0726b88f7831339f07d8cd8d811a2df9e1
hyemco <hyemco@gmail.com>
2023-09-26 12:07:14 +0900

    💄 Design: 매매 일지 목록 DatePicker 커스텀
    - 달력 좌우 가운데 정렬
    - 한글 버전
    - 날짜 포맷 설정
    - 선택시 색 변경

--- antoday_FE/src/components/TradingRecord/template/SearchingDate.tsx
+++ antoday_FE/src/components/TradingRecord/template/SearchingDate.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import DatePicker from "react-datepicker";
+import { ko } from "date-fns/esm/locale";
 import "react-datepicker/dist/react-datepicker.css";
+import '../../../DatePicker.css'
 import styles from './SearchingDate.module.css'
 
 interface SearchInputProps {
@@ -11,7 +13,6 @@ const formatDate = (date: Date) => {
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0');
     const dd = String(date.getDate()).padStart(2, '0');
-  
     return `${yyyy}-${mm}-${dd}`;
 };
 
@@ -34,8 +35,10 @@ const SearchingDate: React.FC<SearchInputProps> = ({ onSearch }) => {
     }, [startDate, endDate]);
 
     return (
-        <div>
+        <div className={styles.dateContainer}>
             <DatePicker
+                locale={ko}
+                dateFormat="yyyy.MM.dd"
                 selected={startDate}
                 onChange={(date: Date | [Date, Date] | null) => setStartDate(date as Date)}
                 selectsStart
@@ -43,17 +46,25 @@ const SearchingDate: React.FC<SearchInputProps> = ({ onSearch }) => {
                 endDate={endDate}
                 placeholderText="시작 날짜"
                 className = {styles.datepicker}
-            />
+                filterDate={(date: Date) => {
+                    return date.getDay() !== 0 && date.getDay() !== 6;
+                }}
+                />
             <span>~</span>
             <DatePicker
+                locale={ko}
+                dateFormat="yyyy.MM.dd"
                 selected={endDate}
                 onChange={(date: Date | [Date, Date] | null) => setEndDate(date as Date)}
                 selectsEnd
                 startDate={startDate}
                 endDate={endDate}
-                minDate={startDate}
+                maxDate={new Date()}
                 placeholderText="종료 날짜"
                 className = {styles.datepicker}
+                filterDate={(date: Date) => {
+                    return date.getDay() !== 0 && date.getDay() !== 6;
+                }}
             />
         </div>
     );
