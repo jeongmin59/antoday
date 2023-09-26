1e76bcd4901a03f1b8ff12de2369ce259dbe9714
subin <osb4469@gmail.com>
2023-09-27 05:05:52 +0900

    🐛 Fix: 페이지 선택 시 지속적으로 재렌더링되는 오류 잡음 & css

--- antoday_FE/src/components/TradingRecord/template/WriteTradingRecord.tsx
+++ antoday_FE/src/components/TradingRecord/template/WriteTradingRecord.tsx
@@ -1,4 +1,4 @@
-  import React, { useState, useEffect } from 'react';
+  import React, { useState, useEffect, useCallback } from 'react';
   import { useNavigate } from 'react-router-dom';
   import DatePicker from "react-datepicker";
   import "react-datepicker/dist/react-datepicker.css";
@@ -228,11 +228,7 @@ import { ko } from "date-fns/esm/locale";
     }, [selectedDate, selectedOption, selectedCompany]);
 
 
-
-    const handleSearchCompany = (keyword: string) => {
-      // setSearchKeyword(keyword);
-      // setSelectedCompany(null);
-      
+    const handleSearchCompany = useCallback((keyword: string) => {
       if (keyword !== '') {
         axios.get(`${import.meta.env.VITE_BACK_API_URL}/api/corp/search`, {
           params: {
@@ -243,7 +239,9 @@ import { ko } from "date-fns/esm/locale";
             Authorization: `Bearer ${token}`
           }
         })
+       
         .then((response) => {
+          // console.log(currentPage)
           const { content, totalPages } = response.data;
           setSearchResults(content);
           setTotalPages(totalPages);
@@ -254,7 +252,8 @@ import { ko } from "date-fns/esm/locale";
       } else {
         // setSearchResults([]);
       }
-    };
+  }, [currentPage, token]); // 의존성 배열에 필요한 변수를 추가하세요.
+  
     
     const adjustPrice = (increment: number) => {
       if (adjustedPrice !== null) {
@@ -265,7 +264,9 @@ import { ko } from "date-fns/esm/locale";
 
     const handlePageChange = (newPage: number) => {
       setCurrentPage(newPage);
+      // console.log(currentPage)
       handleSearchCompany(searchKeyword);
+      // console.log('handlepagechange!')
     };
 
     const handleButtonClick = async () => {
@@ -303,96 +304,103 @@ const resetChooseState = () => {
   // console.log(ownedCompanies)
     return (
       <div className={styles.writecontainer}>
+    <div className={styles.title}>매매기록 작성</div>
+    <div className={styles.card}>
         <div className={styles.horizontal}>
-          <p>날짜</p>
-          <DatePicker
-            locale={ko}
-            dateFormat="yyyy.MM.dd"
-            selected={selectedDate}
-            onChange={(date: Date) => setSelectedDate(date)}
-            filterDate={(date: Date) => {
-                return date.getDay() !== 0 && date.getDay() !== 6;
-            }}
-            placeholderText="날짜 선택"
-            maxDate={initialAdjustedDate}
-          />  
-
-          <button
-            className={`${selectedOption === '매수' ? styles.bold : styles.normal} ${styles.button}`}
-            onClick={() => handleClick('매수')}
-          >
-            매수
-          </button>
-          <button
-            className={`${selectedOption === '매도' ? styles.bold : styles.normal} ${styles.button}`}
-            onClick={() => handleClick('매도')}
-          >
-            매도
-          </button>
-        </div>
-    
-        <div>
-        <div>
-  
-</div>
-{selectedOption === '매수' && <SearchingCompany onSearch={handleSearchCompany} resetChoose={resetChooseState} searchResults={searchResults} selectedCompany={selectedCompany} setSelectedCompany={setSelectedCompany} choose={isChoose} handleSelectCompany={handleSelectCompany} handlePageChange= {handlePageChange} totalPages={totalPages}/>}
-        {selectedCompany && selectedOption === '매도' ? (
-          <div>
-            <div key={selectedCompany?.stockCode} className={styles.corpcontainer}>
-              <img src={selectedCompany?.logoUrl} alt={selectedCompany?.corpName} />
-              <span>{selectedCompany?.corpName}</span>
+            <div className={styles.leftContainer}>
+                <div className={styles.weather}>날짜</div>
+                <div className={styles.datepicker}>
+                    <DatePicker
+                        locale={ko}
+                        dateFormat="yyyy.MM.dd"
+                        selected={selectedDate}
+                        onChange={(date: Date) => setSelectedDate(date)}
+                        filterDate={(date: Date) => {
+                            return date.getDay() !== 0 && date.getDay() !== 6;
+                        }}
+                        placeholderText="날짜 선택"
+                        maxDate={initialAdjustedDate}
+                    />  
+                </div>
             </div>
-          </div>
-        ) : null}
-        {ownedCompanies.length > 0 && selectedOption === '매도' && !selectedCompany ? (
-          <div>
-            <h2>보유한 주식</h2>
-            {ownedCompanies.map((company) => (
-              <div 
-                key={company.stockCode} 
-                className={styles.corpcontainer}
-                onClick={() => handleSelectCompany(company)}
-              >
-                <img src={company.logoUrl} alt={company.corpName} />
-                <span>{company.corpName}</span>
-              </div>
-            ))}
-          </div>
-        ) : (selectedOption === '매도' && ownedCompanies.length === 0 ? (
-          <div>보유주식이 없습니다.</div>
-        ) : null)}
-
-    <>
-      <div>
-        <input
-          type="number"
-          value={stockQuantity === 0 ? "" : stockQuantity}
-          onChange={(e) => {
-            const value = e.target.value;
-            if (value === "") {
-                setStockQuantity(0);
-            } else {
-                setStockQuantity(parseInt(value, 10));
-            }
-        }}
-        />
-        주
-      </div>
-      <div className={styles.horizontal}>
-        <div>평단가</div>
-        <div>
-          <button onClick={() => adjustPrice(-getIncrementValue())}>-</button>
-          {adjustedPrice !== null ? `${adjustedPrice}원` : "0 원"}
-          <button onClick={() => adjustPrice(getIncrementValue())}>+</button>
+            <div className={styles.rightContainer}>
+                <button
+                    className={`${selectedOption === '매수' ? styles.bold : styles.normal} ${styles.button}`}
+                    onClick={() => handleClick('매수')}
+                >
+                    매수
+                </button>
+                <button
+                    className={`${selectedOption === '매도' ? styles.bold : styles.normal} ${styles.button}`}
+                    onClick={() => handleClick('매도')}
+                >
+                    매도
+                </button>
+            </div>
         </div>
-      </div>
-      <button onClick={closeWritePage}>취소</button>
-      <button onClick={handleButtonClick}>추가</button>
-      {alertMessage && <p>{alertMessage}</p>}
-    </>
+    <div className={styles.parentContainer}>
+        {selectedOption === '매수' && <SearchingCompany onSearch={handleSearchCompany} resetChoose={resetChooseState} searchResults={searchResults} selectedCompany={selectedCompany} setSelectedCompany={setSelectedCompany} choose={isChoose} handleSelectCompany={handleSelectCompany} handlePageChange= {handlePageChange} totalPages={totalPages}/>}
+        
+        {selectedCompany && selectedOption === '매도' && (
+            <div className={styles.corpcontainer}>
+                <img src={selectedCompany.logoUrl} alt={selectedCompany.corpName} />
+                <span>{selectedCompany.corpName}</span>
+            </div>
+        )}
 
+        {ownedCompanies.length > 0 && selectedOption === '매도' && !selectedCompany && (
+            <div>
+                <h2 className={styles.ownedTitle}>보유한 주식</h2>
+                {ownedCompanies.map((company) => (
+                    <div 
+                        key={company.stockCode}
+                        className={styles.corpcontainer}
+                        onClick={() => handleSelectCompany(company)}
+                    >
+                        <img src={company.logoUrl} alt={company.corpName} />
+                        <span>{company.corpName}</span>
+                    </div>
+                ))}
+            </div>
+        )}
+
+        {selectedOption === '매도' && ownedCompanies.length === 0 && <div>보유주식이 없습니다.</div>}
+        
+        <div className={styles.inputContainer}>
+            <input
+                type="number"
+                className={styles.quantityinput}
+                value={stockQuantity === 0 ? "" : stockQuantity}
+                onChange={(e) => {
+                    const value = e.target.value;
+                    if (value === "") {
+                        setStockQuantity(0);
+                    } else {
+                        setStockQuantity(parseInt(value, 10));
+                    }
+                }}
+            />
+            주
         </div>
-      </div>
+
+        <div className={styles.horizontal2}>
+            <div className={styles.weather}>평단가</div>
+            <div className={styles.priceAdjust}>
+                <button className={styles.button2} onClick={() => adjustPrice(-getIncrementValue())}>-</button>
+                {adjustedPrice !== null ? `${adjustedPrice}원` : "0 원"}
+                <button className={styles.button2} onClick={() => adjustPrice(getIncrementValue())}>+</button>
+            </div>
+        </div>
+        </div>
+        <div className={styles.buttonsContainer}>
+            <button onClick={closeWritePage} className={styles.buttoncancel}>취소</button>
+            <button onClick={handleButtonClick} className={styles.buttonadd}>추가</button>
+        </div>
+
+        {alertMessage && <p>{alertMessage}</p>}
+    </div>
+</div>
+
     );
   };
 
