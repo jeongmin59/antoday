b4c2e1925d634056dc99021016736cf3f960fbd0
SEOIHYEON <tjwn1408@naver.com>
2023-09-26 21:16:43 +0900

    :sparkles: Feat: 종목 정보 api 연동

--- antoday_FE/src/components/StockInfo/module/StockInformation.tsx
+++ antoday_FE/src/components/StockInfo/module/StockInformation.tsx
@@ -8,19 +8,23 @@ interface StockInformationProps {
 
 const StockInformation: React.FC<StockInformationProps> = ({ corpInfo }) => {
   console.log("종목정보 컴포넌트에서", corpInfo);
-  const [titleText, setTitle] = useState<string>("");
-  const [contentText, setContent] = useState<number>(0);
+  const BPS = corpInfo.BPS;
+  const EPS = corpInfo.EPS;
+  const PBR = corpInfo.PBR;
+  const PER = corpInfo.PER;
+  const ROA = corpInfo.ROA;
+  const ROE = corpInfo.ROE;
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.title}>종목정보</div>
       <div className={styles.StockInfoContainer}>
-        <InfoCardComponent title={titleText} number={contentText} />
-        <InfoCardComponent title={titleText} number={contentText} />
-        <InfoCardComponent title={titleText} number={contentText} />
-        <InfoCardComponent title={titleText} number={contentText} />
-        <InfoCardComponent title={titleText} number={contentText} />
-        <InfoCardComponent title={titleText} number={contentText} />
+        <InfoCardComponent title={"BPS"} number={BPS} />
+        <InfoCardComponent title={"EPS"} number={EPS} />
+        <InfoCardComponent title={"PBR"} number={PBR} />
+        <InfoCardComponent title={"PER"} number={PER} />
+        <InfoCardComponent title={"ROA"} number={ROA} />
+        <InfoCardComponent title={"ROE"} number={ROE} />
       </div>
     </div>
   );
