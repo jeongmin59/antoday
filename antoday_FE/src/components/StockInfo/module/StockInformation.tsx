b9d1f7b156d8fa7f1426a23469f31684410d41f2
SEOIHYEON <tjwn1408@naver.com>
2023-09-26 10:49:41 +0900

    :lipstick: Design: app.css 수정

--- antoday_FE/src/components/StockInfo/module/StockInformation.tsx
+++ antoday_FE/src/components/StockInfo/module/StockInformation.tsx
@@ -3,22 +3,20 @@ import styles from "./StockInformation.module.css";
 import InfoCardComponent from "../atom/InfoCardComponent";
 
 interface StockInformationProps {
-  corpInfo : string[];
+  corpInfo: string[];
 }
 
-const StockInformation: React.FC<StockInformationProps> = ({
-  corpInfo
-}) => {
+const StockInformation: React.FC<StockInformationProps> = ({ corpInfo }) => {
   const [titleText, setTitle] = useState<string>("");
   const [contentText, setContent] = useState<number>(0);
-  // console.log('궁금해',corpInfo)
+  console.log("궁금해", corpInfo);
   return (
     <div className={styles.mainContainer}>
       <div className={styles.title}>종목정보</div>
       <div className={styles.StockInfoContainer}>
-        {corpInfo.map((res)=>(
+        {/* {corpInfo.map((res)=>(
           //이거맞아?
-        ))}
+        ))} */}
         <InfoCardComponent title={titleText} number={contentText} />
         <InfoCardComponent title={titleText} number={contentText} />
         <InfoCardComponent title={titleText} number={contentText} />
