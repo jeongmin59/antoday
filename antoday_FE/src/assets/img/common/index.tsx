135afe5c87cca6d6562419e482f25eec39d039c1
subin <osb4469@naver.com>
2023-10-04 17:53:01 +0900

    💄 Design: 로딩 gif 추가 및 조건부 렌더링

--- antoday_FE/src/assets/img/common/index.tsx
+++ antoday_FE/src/assets/img/common/index.tsx
@@ -3,5 +3,6 @@ import homeImage from './home.png';
 import tradingImage from './trading.png'
 import headerImage from './header_image.png'
 import memo from './memo.png'
-export { homeImage, companyImage, headerImage, memo };
+import loading from './loading.gif'
+export { homeImage, companyImage, headerImage, memo, loading };
 
