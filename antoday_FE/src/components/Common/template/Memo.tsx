d74b51d729815f0b6d9c35199e0f631380a419a8
SEOIHYEON <tjwn1408@naver.com>
2023-10-04 17:41:29 +0900

    :lipstick: Design: 메모 css 수정

--- antoday_FE/src/components/Common/template/Memo.tsx
+++ antoday_FE/src/components/Common/template/Memo.tsx
@@ -1,19 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import styles from './Memo.module.css';
-import axios from 'axios';
-import { useRecoilValue } from 'recoil';
-import { accessTokenAtom } from '../../../recoil/auth';
+import React, { useState, useEffect } from "react";
+import styles from "./Memo.module.css";
+import axios from "axios";
+import { useRecoilValue } from "recoil";
+import { accessTokenAtom } from "../../../recoil/auth";
 
 interface MemoProps {
   isOpen?: boolean; // className 속성을 받을 수 있도록 정의
 }
 
 const Memo: React.FC<MemoProps> = ({ isOpen }) => {
-  
-  const [memoText, setMemoText] = useState('');
+  const [memoText, setMemoText] = useState("");
   const token = useRecoilValue(accessTokenAtom);
   const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
-  const memoContainerClassName = isOpen ? `${styles.textArea} ${styles.open}` : styles.textArea;
+  const memoContainerClassName = isOpen
+    ? `${styles.textArea} ${styles.open}`
+    : styles.textArea;
 
   useEffect(() => {
     const getMemo = async () => {
@@ -28,14 +29,14 @@ const Memo: React.FC<MemoProps> = ({ isOpen }) => {
         );
         setMemoText(response.data.memo);
       } catch (error) {
-        console.error('메모 가져오기 에러', error);
+        console.error("메모 가져오기 에러", error);
       }
     };
 
     getMemo();
 
     return () => {
-      setMemoText('');
+      setMemoText("");
     };
   }, [token]);
 
@@ -57,7 +58,9 @@ const Memo: React.FC<MemoProps> = ({ isOpen }) => {
   const handleUpdateMemo = async () => {
     try {
       await axios.put(
-        `${import.meta.env.VITE_BACK_API_URL}/api/memo?memo=${encodeURIComponent(memoText)}`,
+        `${
+          import.meta.env.VITE_BACK_API_URL
+        }/api/memo?memo=${encodeURIComponent(memoText)}`,
         { memo: memoText },
         {
           headers: {
@@ -65,20 +68,22 @@ const Memo: React.FC<MemoProps> = ({ isOpen }) => {
           },
         }
       );
-      console.log('메모가 성공적으로 수정되었습니다');
+      console.log("메모가 성공적으로 수정되었습니다");
     } catch (error) {
-      console.error('메모 수정이 실패하였습니다', error);
+      console.error("메모 수정이 실패하였습니다", error);
     }
   };
 
   return (
-          <textarea
-            rows={5}
-            cols={30}
-            value={memoText}
-            onChange={handleMemoChange}
-            className={memoContainerClassName}
-          />
+    <div className={memoContainerClassName}>
+      <textarea
+        rows={5}
+        cols={30}
+        value={memoText}
+        onChange={handleMemoChange}
+        className={styles.inputBox}
+      />
+    </div>
   );
 };
 
