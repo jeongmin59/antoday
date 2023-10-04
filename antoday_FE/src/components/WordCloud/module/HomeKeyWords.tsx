ae380bbcfcff17c9d50942488ba7e0603da6ef25
Pangpyo <kkp0639@gmail.com>
2023-10-04 17:40:20 +0900

    ✨ Feat: 워드클라우드 클릭이벤트 추가중

--- antoday_FE/src/components/WordCloud/module/HomeKeyWords.tsx
+++ antoday_FE/src/components/WordCloud/module/HomeKeyWords.tsx
@@ -9,16 +9,18 @@ interface WordCloudData {
   value: number;
 }
 
+
+
 const HomeKeyWords: React.FC = () => {
   // 워드 클라우드에 표시할 데이터 예시
   const [words, setWords] = useState<WordCloudData[]>([]);
   const [corps, setCorps] = useState(null);
-  const [chartWidth, setChartWidth] = useState(window.innerWidth * 0.8);
-
+  const [chartWidth, setChartWidth] = useState(window.innerWidth * 0.75);
   const updateChartSize = () => {
-    setChartWidth(window.innerWidth * 0.8);
+    setChartWidth(window.innerWidth * 0.75);
   };
   
+
   useEffect(() => {
     window.addEventListener("resize", updateChartSize);
     getWordCloudData();
@@ -27,7 +29,20 @@ const HomeKeyWords: React.FC = () => {
     };
   }, []);
 
-
+  const bubbleClick = async (label:string) =>{
+    console.log(label, "클릭");
+    try {
+      const response = await axios.get<WordCloudData[]>(
+        import.meta.env.VITE_DATA_API_URL + '/keyword/' + label
+        );
+        const data = response.data;
+        setWords(data.cloud);
+        setCorps(data.corps);
+        console.log("클릭호출")
+    } catch (error) {
+      console.error("호출 실패 :" , error);
+    }
+  }
   const getWordCloudData = async () => {
     try {
       const response = await axios.get<WordCloudData[]>(
@@ -36,28 +51,34 @@ const HomeKeyWords: React.FC = () => {
         const data = response.data;
         setWords(data.cloud);
         setCorps(data.corps);
+        console.log("호출")
     } catch (error) {
       console.error("호출 실패 :" , error);
     }
   };
-0
-  console.log('기업정보 5개 담긴 배열', corps)
   return (
     <div>
-      <BubbleChart
-        data={words}
-        width={chartWidth} 
-        height={700} 
-        showLegend={false}
-        labelFont={{
-          size: 15,
-          color: '#fff',
-          weight: 'bold',
-        }}
-        valueFont={{
-          size: 0,
-        }}
-      />
+      <div className={styles.bubbleChartContainer}>
+        <BubbleChart
+          bubbleClickFunc={bubbleClick}
+          graph= {{
+            zoom: 0.9,
+          }}
+          padding={10}
+          data={words}
+          width={chartWidth} 
+          height={chartWidth} 
+          showLegend={false}
+          labelFont={{
+            size: 15,
+            color: '#fff',
+            weight: 'light',
+          }}
+          valueFont={{
+            size: 0,
+          }}
+        />
+      </div>
       <HomeKeyWordsCompany/>
     </div>
   );
