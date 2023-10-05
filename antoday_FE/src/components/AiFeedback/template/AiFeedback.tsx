2bb513c6dfb9486304e9dda4e5dba957c3b3690e
SEOIHYEON <tjwn1408@naver.com>
2023-10-06 02:42:53 +0900

    :bug: Fixed: ai피드백 버그 수정

--- antoday_FE/src/components/AiFeedback/template/AiFeedback.tsx
+++ antoday_FE/src/components/AiFeedback/template/AiFeedback.tsx
@@ -17,11 +17,10 @@ const AiFeedback: React.FC<TradingRecord> = ({
   aiAnalyze,
   stockCode,
 }) => {
-  console.log("분석내용이있나요?", aiAnalyze);
-
   const { tradePk } = useParams();
-  const [results, setResults] = useState(aiAnalyze);
-  const queryClient = useQueryClient();
+  const [results, setResults] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
 
   const fetchAiFeedback = async (tradePk: string | undefined) => {
     try {
@@ -41,70 +40,59 @@ const AiFeedback: React.FC<TradingRecord> = ({
       );
       console.log("AI 결과분석 fetch함수안일때", response.data);
       setResults(response.data);
-
+      setIsLoading(false);
       return response.data;
     } catch (error) {
+      setIsError(true);
       throw new Error("AI 결과분석 실패");
+    } finally {
+      window.location.reload();
     }
   };
 
-  const {
-    data: aiFeedback,
-    isLoading,
-    isError,
-  } = useQuery("aiFeedback", () => fetchAiFeedback(tradePk), {
-    enabled: false,
-  });
-
   const handleClick = () => {
     console.log("핸들클릭 실행됨");
-    queryClient.refetchQueries("aiFeedback");
+    setIsLoading(true);
+    fetchAiFeedback(tradePk);
   };
 
-  useEffect(() => {
-    console.log("results가 바뀌면,", results);
-    console.log("AI 결과분석 완료 useEffect안", results);
-  }, [results]);
+  useEffect(() => {}, [results]);
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.title}>AI피드백</div>
       <div className={styles.content}>
-        {results ? (
-          <div className={styles.subcontainer}>
+        {isLoading ? (
+          <div>
             <img className={styles.antimage} src={AntDefault} alt="개미" />
-            <div className={styles.feedback}>{results}</div>
+            <div className={styles.feedback}>
+              <LoadingSpinner />
+            </div>
+          </div>
+        ) : isError ? (
+          <div>
+            <img
+              className={styles.antbutton}
+              onClick={handleClick}
+              src={AntDefault}
+              alt="개미"
+            />
+            <div className={styles.feedback}>
+              피드백을 불러오는 도중 오류가 발생했습니다.
+            </div>
           </div>
         ) : (
-          <>
-            {isLoading ? (
-              <>
-                <img className={styles.antimage} src={AntDefault} alt="개미" />
-                <div className={styles.feedback}>
-                  <LoadingSpinner />
-                </div>
-              </>
-            ) : isError ? (
-              <>
-                <img className={styles.antimage} src={AntDefault} alt="개미" />
-                <div className={styles.feedback}>
-                  피드백을 불러오는 도중 오류가 발생했습니다.
-                </div>
-              </>
-            ) : (
-              <div className={styles.subContainer}>
-                <img
-                  className={styles.antbutton}
-                  src={AntDefault}
-                  alt="개미"
-                  onClick={handleClick}
-                />
-                <div className={styles.feedback}>
-                  개미를 눌러 피드백을 받아보세요
-                </div>
-              </div>
-            )}
-          </>
+          <div>
+            <img
+              className={styles.antbutton}
+              src={AntDefault}
+              alt="개미"
+              onClick={handleClick}
+            />
+            <div className={styles.feedback}>
+              개미를 눌러 피드백을 받아보세요
+            </div>
+          </div>
         )}
       </div>
     </div>
