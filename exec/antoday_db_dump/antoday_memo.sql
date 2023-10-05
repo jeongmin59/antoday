e77eb831113c4f50f0c2ac5a3cb30113a3f1969a
hyemco <hyemco@gmail.com>
2023-10-06 00:28:17 +0900

    📝 Docs: 포팅 매뉴얼 및 DB Dump 데이터 추가

---
+++ exec/antoday_db_dump/antoday_memo.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: antoday
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `memo`
+--
+
+DROP TABLE IF EXISTS `memo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `memo` (
+  `memo_pk` bigint NOT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `memo` text,
+  `updated_at` datetime(6) DEFAULT NULL,
+  `social_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`memo_pk`),
+  KEY `FK5dq9i08j1qe6k9uyr1aa7bq3v` (`social_id`),
+  CONSTRAINT `FK5dq9i08j1qe6k9uyr1aa7bq3v` FOREIGN KEY (`social_id`) REFERENCES `user` (`social_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `memo`
+--
+
+LOCK TABLES `memo` WRITE;
+/*!40000 ALTER TABLE `memo` DISABLE KEYS */;
+INSERT INTO `memo` VALUES (2,'2023-10-05 20:29:49.432364','삼성전자 - 갤럭시S24 1, 2월 출시 예정','2023-10-05 20:36:59.234044',3015700161);
+/*!40000 ALTER TABLE `memo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-05 23:57:57
